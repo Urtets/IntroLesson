f08c289c2f4fdcf5d66d629c33191033daae5f73
Юрий <144734719+Urtets@users.noreply.github.com>
2024-08-18 17:53:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# "1st program"
+print(9 ** 0.5 * 5)
+
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+a = 2 * 2 + 2
+print(a)
+b = 2 * (2 + 2)
+print(b)
+print(a == b)
+
+# 4th program
+
+s = '123.456'
+f = float(s)
+number = int(f * 10)
+number = number % 1230
+print(number)
+
